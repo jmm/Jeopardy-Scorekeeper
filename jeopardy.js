1c625616fcffb559d46c4828f0639abbf8c2822a
Jesse McCarthy <git_commits@jessemccarthy.net>
2012-03-26 04:29:28 -0400

    Public release 1.0b7.

--- jeopardy.js
+++ jeopardy.js
@@ -1,12 +1,16 @@
 
+
 /*
 
-Copyright © 2010-2011 Jesse McCarthy <http://jessemccarthy.net/>
+Copyright © 2010-2012 Jesse McCarthy <http://jessemccarthy.net/>
 
-This file is part of the Jeopardy Scorekeeper software.  "JEOPARDY!" is a trademark of Jeopardy Productions, Inc.  This software is not endorsed by, sponsored by, or affiliated with Jeopardy Productions, Inc.
+This file is part of the Jeopardy Scorekeeper software.  "JEOPARDY!"
+is a trademark of Jeopardy Productions, Inc.  This software is not
+endorsed by, sponsored by, or affiliated with Jeopardy Productions,
+Inc.
 
-This software may be used under the MIT (aka X11) license or Simplified BSD
-(aka FreeBSD) license.  See LICENSE.
+This software may be used under the MIT (aka X11) license or
+Simplified BSD (aka FreeBSD) license.  See LICENSE.
 
 */
 
@@ -34,4 +38,3 @@ $( document ).ready( function () {
 
 
 var Jeopardy = {};
-
